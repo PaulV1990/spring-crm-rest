2313d9147f7f2ce8a3d32ebe079ef0120158103e
17KryptoVos <17kryptovos@protonmail.com>
2020-08-12 15:16:35 +0200

    - Added API for specific customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +24,17 @@ public class CustomerRestController {
         this.customerService = customerService;
     }
 
-    // ADd mapping for GET /customers
+    // Add mapping for GET /customers
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
-        // Delegate to the get the Customer Service
+        // Delegate to the get all Customers Service
         return customerService.getCustomers();
+    }
+
+    // Add mapping for GET /customers//{customerId}
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        // Delegate to the get the Customer Service
+        return customerService.getCustomer(customerId);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,7 +4,9 @@
 <h3>Spring CRM REST Demo</h3>
 
 <hr>
-
+<a href="${pageContext.request.contextPath}/api/customers">Get all customers with the API</a>
+<br>
+<a href="${pageContext.request.contextPath}/api/customers/1">Get specific customer with the API</a>
 
 </body>
 </html>
